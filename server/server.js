31d9ac92a8b6ecb24aa7f85459c0d039d5a3e13e
jide olaniyan <jidd32@gmail.com>
2018-02-18 00:06:08 +0400

    add POST todo ROUTE

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+let {mongoose} = require('./db/mongoose')
+let {Todo} = require('./models/todo')
+let {User} = require('./models/user')
+let app = express();
+
+app.use(bodyParser.json());  //not a custom middleware
+
+app.post('/todos', (req,res)=> {
+       let todo = new Todo({
+        text: req.body.text 
+       });
+
+       todo.save().then((doc)=>{
+            res.send(doc)
+       },(e)=>{
+           res.status(400).send(e)
+       })
+       
+})
+
+app.listen(3000, ()=>{
+    console.log('started on port 3000')
+})
+
+
+
+
+
+
+
