787398772139d7054e241d208d2da20c95656531
jide olaniyan <jidd32@gmail.com>
2018-02-25 00:58:30 +0400

    review user model with validation

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+
 mongoose.PromiseProvider = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
+
 
 
 module.exports = {mongoose}
--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,36 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+
+
 let User = mongoose.model('User', {
     email: {
        type: String,
        required: true,
        minlength: 1,
-       trim: true
-    }
-})
+       trim: true,
+       unique: true,
+       validate: {
+           validator: validator.isEmail,
+           message: '{VALUE} is not a valid email'
+       }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokend: [
+        {
+            access: {
+                type: String,
+                required: true
+            },
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+});
 
 module.exports = {User}
